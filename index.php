bdefbb2e24dd5fc4fe74b614ebdaf9b89e3ad712
Shweta Mohanty <shweta.mohanty@hpe.com>
2019-04-08 16:01:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>My Maze Application</title>
+    <title>My Space Application</title>
     <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
     <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
     <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
